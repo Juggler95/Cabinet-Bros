8627575717f2b3e8797ffd56129c85184c29b97d
Juggler95 <fireboltisaac@gmail.com>
2025-05-03 18:13:32 -0700

    removed extra code in script.js

--- script.js
+++ script.js
@@ -12,17 +12,3 @@ document.querySelectorAll('.nav-item').forEach((n) =>
     navMenu.classList.remove('active');
   })
 );
-
-function sendEmail() {
-  Email.send({
-    Host: 'smtp.gmail.com',
-    Username: 'sender@email_address.com',
-    Password: 'Enter your password',
-    To: 'fireboltisaac@gmail.com',
-    From: 'sender@email_address.com',
-    Subject: 'Sending Email using javascript',
-    Body: 'Well that was easy!!',
-  }).then(function (message) {
-    alert('mail sent successfully');
-  });
-}
